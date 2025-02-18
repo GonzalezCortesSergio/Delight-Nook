33d8c16b7165d7762ad370b44ecfc6181a9f70b0
Sergio González <gonzalez.coser24@triana.salesianos.edu>
2025-02-18 13:53:27 +0100

    First commit

---
+++ src/main/java/com/salesianostriana/dam/delight_nook/DelightNookApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.delight_nook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelightNookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelightNookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salesianostriana/dam/delight_nook/DelightNookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.delight_nook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelightNookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
