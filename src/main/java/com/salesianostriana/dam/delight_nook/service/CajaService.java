d686cfb432d9eec4cbb45f22ae57f2bb0e3c7113
GonzalezCortesSergio <gonzalez.coser24@triana.salesianos.edu>
2025-02-20 19:50:46 +0100

    Terminado

--- src/main/java/com/salesianostriana/dam/delight_nook/service/CajaService.java
+++ src/main/java/com/salesianostriana/dam/delight_nook/service/CajaService.java
@@ -1,9 +1,13 @@
 package com.salesianostriana.dam.delight_nook.service;
 
 import com.salesianostriana.dam.delight_nook.dto.CreateCajaDto;
+import com.salesianostriana.dam.delight_nook.dto.GetCajaDto;
+import com.salesianostriana.dam.delight_nook.error.CajaNotFoundException;
 import com.salesianostriana.dam.delight_nook.model.Caja;
 import com.salesianostriana.dam.delight_nook.repository.CajaRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,5 +22,15 @@ public class CajaService {
                 .nombre(cajaDto.nombre())
                 .dineroCaja(cajaDto.dineroCaja())
                 .build());
+    }
+
+    public Page<GetCajaDto> findAll(Pageable pageable) {
+
+        Page<Caja> result = cajaRepository.findAll(pageable);
+
+        if(result.isEmpty())
+            throw new CajaNotFoundException("No se han encontrado cajas");
+
+        return result.map(GetCajaDto::of);
     }
 }
