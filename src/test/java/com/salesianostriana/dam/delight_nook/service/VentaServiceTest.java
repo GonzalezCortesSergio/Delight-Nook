37897c7594573b27086b7f4c47618f3e8ccc3e62
Sergio González <sergio.gonzalezc@alten.es>
2025-05-21 19:13:40 +0200

    feat: test implementado correctamente

--- src/test/java/com/salesianostriana/dam/delight_nook/service/VentaServiceTest.java
+++ src/test/java/com/salesianostriana/dam/delight_nook/service/VentaServiceTest.java
@@ -3,6 +3,7 @@ package com.salesianostriana.dam.delight_nook.service;
 import com.salesianostriana.dam.delight_nook.dto.producto.ProductoCantidadDto;
 import com.salesianostriana.dam.delight_nook.model.Caja;
 import com.salesianostriana.dam.delight_nook.model.Gestiona;
+import com.salesianostriana.dam.delight_nook.model.Producto;
 import com.salesianostriana.dam.delight_nook.model.Venta;
 import com.salesianostriana.dam.delight_nook.repository.CajaRepository;
 import com.salesianostriana.dam.delight_nook.repository.ProductoRepository;
@@ -13,7 +14,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -22,9 +23,9 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
+@SpringBootTest(classes = VentaServiceTest.class)
 class VentaServiceTest {
 
     @Mock
@@ -50,12 +51,19 @@ class VentaServiceTest {
 
     private Gestiona gestiona;
 
+    private Producto producto;
+
     private ProductoCantidadDto productoCantidadDto;
 
     @BeforeEach
     void setUp() {
-        MockitoAnnotations.openMocks(this);
-
+        producto = Producto.builder()
+                .id(1L)
+                .nombre("Zapatos")
+                .descripcion("Zapatikos")
+                .precioUnidad(12.23)
+                .proveedor("Zara")
+                .build();
         cajero = Cajero.builder()
                 .id(UUID.randomUUID())
                 .nombreCompleto("Manolo Fernández")
@@ -78,21 +86,25 @@ class VentaServiceTest {
                 .nombreCajero(cajero.getNombreCompleto())
                 .build();
 
-        when(cajaRepository.findByCajeroSesion(cajero.getUsername())).thenReturn(Optional.of(caja));
-        when(ventaRepository.findVentaNotFinalizadaByCajaId(caja.getId())).thenReturn(List.of(venta));
+        productoCantidadDto = new ProductoCantidadDto(1L, 2);
     }
 
     @Test
     void create() {
         when(ventaRepository.save(any(Venta.class))).thenReturn(venta);
+        when(cajaRepository.findByCajeroSesion(cajero.getUsername())).thenReturn(Optional.of(caja));
+        when(ventaRepository.findVentaNotFinalizadaByCajaId(caja.getId())).thenReturn(List.of(venta));
+        when(productoRepository.findById(1L)).thenReturn(Optional.of(producto));
+        when(stockRepository.cantidadProductosStock(1L)).thenReturn(Optional.ofNullable(2));
 
         Venta result = ventaService.create(cajero, productoCantidadDto);
 
-        verify(cajaRepository).save(caja);
-        verify(ventaRepository).save(any(Venta.class));
-
-        assertNotNull(result);
-        assertEquals("Manolo Fernández", result.getNombreCajero());
+        assertAll(
+                () -> verify(cajaRepository, times(0)).save(any(Caja.class)),
+                () -> verify(ventaRepository, times(1)).save(any(Venta.class)),
+                () -> assertNotNull(result),
+                () -> assertEquals("Manolo Fernández", result.getNombreCajero())
+        );
 
     }
 }
