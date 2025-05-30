7497d11edd98e5b8498ac3d7551853241117f722
Sergio González <gonzalez.coser24@triana.salesianos.edu>
2025-05-30 13:58:37 +0200

    fix: error en la relación de categoría con categorías hijas

--- src/main/java/com/salesianostriana/dam/delight_nook/repository/CategoriaRepository.java
+++ src/main/java/com/salesianostriana/dam/delight_nook/repository/CategoriaRepository.java
@@ -1,10 +1,14 @@
 package com.salesianostriana.dam.delight_nook.repository;
 
 import com.salesianostriana.dam.delight_nook.model.Categoria;
+import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 
@@ -16,4 +20,14 @@ public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
             WHERE c.categoriaPadre IS NULL
             """)
     Page<Categoria> findAllCategoriaBase(Pageable pageable);
+
+    @Override
+    @Nonnull
+    @Query("""
+            SELECT c
+            FROM Categoria c
+            LEFT JOIN FETCH c.categoriasHijas
+            WHERE c.id = :aLong
+            """)
+    Optional<Categoria> findById(@Nullable Long aLong);
 }
