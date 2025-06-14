8bdcdeedd2af410702143ae6b5280934c98d804b
Sergio González <gonzalez.coser24@triana.salesianos.edu>
2025-06-14 22:48:46 +0200

    casi se me olvida quitar un cambio

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -28,10 +28,7 @@ import { ProductosListPageComponent } from './pages/admin/productos-list-page/pr
 import { ProductoDetailsPageComponent } from './pages/admin/producto-details-page/producto-details-page.component';
 import { ModalDeleteProductoComponent } from './components/admin/modal-delete-producto/modal-delete-producto.component';
 import { RegisterProductoPageComponent } from './pages/admin/register-producto-page/register-producto-page.component';
-<<<<<<< HEAD
 import { EditProductoPageComponent } from './pages/admin/edit-producto-page/edit-producto-page.component';
-=======
->>>>>>> main
 
 
 
